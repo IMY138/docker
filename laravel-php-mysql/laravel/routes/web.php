b4a965a7166bfeab2a07ecd15b1bdbfdafd024fd
IMY138 <2016004@s.asojuku.ac.jp>
2022-11-24 15:49:33 +0900

    bbs変更

--- laravel-php-mysql/laravel/routes/web.php
+++ laravel-php-mysql/laravel/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('posts', 'PostController', ['only' => ['index', 'show', 'create', 'store', 'destroy']]);
+Route::get('posts/edit/{id}', 'PostController@edit');
+Route::post('posts/edit/{id}', 'PostController@update');
